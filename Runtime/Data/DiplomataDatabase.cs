d1a796392b9af8baf8f337d383fa15b4f02a18c7
Bruno Araujo <araujo.contato@gmail.com>
2019-06-06 16:09:24 -0300

    Get directly the data Resource in a Singleton

--- Runtime/Data/DiplomataDatabase.cs
+++ Runtime/Data/DiplomataDatabase.cs
@@ -1,22 +1,32 @@
 using System.IO;
 using LavaLeak.Diplomata.Helpers;
+using UnityEngine;
 
 namespace LavaLeak.Diplomata
 {
-    [Resources("Diplomata/DiplomataData")]
-    public class DiplomataDatabase : Resource<DiplomataDatabase>
+    public class DiplomataDatabase : ScriptableObject
     {
-        [ReadOnly]
-        public DiplomataData data;
+        private static readonly string PATH = Path.Combine(PathHelper.DIPLOMATA, "DiplomataData");
+        public DiplomataData Data { get; } = new DiplomataData();
 
-        public static void CreateFile()
+        public static DiplomataDatabase Instance
+        {
+            get
+            {
+                var instance = Resources.Load<DiplomataDatabase>(PATH);
+                return instance == null ? CreateFile() : instance;
+            }
+        }
+
+        private static DiplomataDatabase CreateFile()
         {
 #if UNITY_EDITOR
             var content = CreateInstance<DiplomataDatabase>();
             Directory.CreateDirectory(PathHelper.DIPLOMATA);
             Directory.CreateDirectory(PathHelper.LOCALE);
-            UnityEditor.AssetDatabase.CreateAsset(content, Path.Combine(PathHelper.DIPLOMATA, "DiplomataData.asset"));
+            UnityEditor.AssetDatabase.CreateAsset(content, $"{PATH}.asset");
 #endif
+            return Resources.Load<DiplomataDatabase>(PATH);
         }
     }
 }
